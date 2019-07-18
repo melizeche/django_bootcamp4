4136875face8864ea9d3d2ba006d659c8fceaa7d
Marcelo Elizeche Landó <melizeche@gmail.com>
2019-07-18 18:26:29 -0400

    Lo que hicimos en clase

--- pendientes/models.py
+++ pendientes/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Tarea(models.Model):
+class Tarea(models.Model): # La clase Tarea hereda metodos del la clase models.Model
     titulo = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
     descripcion = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
     estado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Tarea: " + self.titulo
--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pendientes.models import Tarea # Importamos la clase Tarea de models.py
+from random import randint
 # Create your views here.
 
 def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+    listita = Tarea.objects.all() #consultamos la BD y guardamos todos
+                                # los registros de la tabla Tarea
+    persona = {                 # como objetos y guardamos en listita
+        "nombre" : "Marce", 
+        "edad":33,
+        "hobbies": ["rugby","comer pan","cortar patas de dinos","comer chocolate","jugar pool"],
+        "lista_tareas": listita, # agregamos la clave lista_tareas
+        }                        # al diccionario de contenido con la
+                                # lista de tareas p/ mandar al template
+    return render(request, 'inicio.html', persona)
+
+def tarea(request):
+    numero =  str(randint(500,999))
+    print("Hola!!!!!", numero)
+    return  HttpResponse("Hola soy la vista tarea" + numero)
+
+def aleatorio(request):
+    numero =  str(randint(1,999))
+    respuesta = "El numero ganador es: " + numero
+    return HttpResponse(respuesta)
+
+# Crear la vista/funcion tarea y conectar con la direccion
+# /tareas en el archivo urls.py
+# despues ir al navegador y abrir http:....../tareas
+
+# Crear una vista respuestas que retorne un texto cuando
+# en el navegador entremos a http:...../info
+# Pista crear la funcion/vista en views.py y conectar en
+# urls.py usando path(.....)
